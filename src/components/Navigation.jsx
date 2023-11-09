5bf5ea42f1b3ddf7d757ddc8c99d49dab1d849e8
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-09 19:04:03 +0200

    add info for every Team from API
    add info from API

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ return (
 <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand as={Link} to='/'>Navbar</Navbar.Brand>
+          <Navbar.Brand  as={Link} to='/'>Efbet Лига</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to='/teams'>Teams</Nav.Link>
--- src/components/TeamStat.jsx
+++ src/components/TeamStat.jsx
@@ -7,12 +7,14 @@ const key = '2844dc51f890f24149f95bc2b52aec45319e12cc47db66e1e3d30e2ad073cded';
 const TeamStat = ()=>{
     const [teamInfo,setTeamInfo] = useState({});
     const [logoUrl, setLogoUrl] = useState('')
+    const [stadion,setStadion] = useState({})
     const {id} =useParams();
     useEffect(() => {
         fetch(`${baseUrl}&APIkey=${key}&team_id=${id}`)
             .then(res => res.json())
             .then(data => {
                 setTeamInfo(data[0]);
+                setStadion(data[0].venue)
                 setLogoUrl(data[0].team_badge);
             });
 
@@ -22,6 +24,11 @@ const TeamStat = ()=>{
 <h1>{teamInfo.team_name}</h1>
             <p>Founded: {teamInfo.team_founded}</p>
             <p>Country: {teamInfo.team_country}</p>
+            <p>Town: {stadion.venue_city}</p>
+            <p>Stadion: {stadion.venue_name}</p>
+            <p>Capacity: {stadion.venue_capacity}</p>
+            <p>Adress : {stadion.venue_address}</p>
+         
             {logoUrl && <img src={logoUrl} alt="Team Logo" />}
 </div>
     );
