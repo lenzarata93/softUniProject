950e4f02cf3fbc0857a28462b42ba7310f09894c
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2023-11-09 10:39:53 +0200

    Add router with routes
    ROUTER AND BASE ROUTES with Refresh of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Navigation from "./components/Navigation"
+import {Route, Routes} from 'react-router-dom'
+import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+
 
 
 function App() {
 
   return (
     <>
+<Navigation />
+<Routes>
+<Route path="/" element={<Home />}></Route>
+<Route path="/about" element ={<About />}></Route>
+<Route path="/contacts" element= {<Contacts />}></Route>
+</Routes>
 
     </>
   )
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+const Navigation = ()=>{
+return (
+<nav>
+  <a href="/">HOME</a>
+  <a href="/about">ABOUT</a>
+  <a href="/contacts">CONTACTS</a>
+</nav>
+);
+
+};
+export default Navigation;
